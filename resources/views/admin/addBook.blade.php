899d867e6c0486806036bf0df905ef283d636c10
kealurullamas <36137954+kealurullamas@users.noreply.github.com>
2018-03-19 06:16:03 +0800

    add_book_form

--- resources/views/admin/addBook.blade.php
+++ resources/views/admin/addBook.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="container addbook-container">
         <h1>Add Book</h1>
-<<<<<<< HEAD
         {!!Form::open(['action'=>'BooksController@store','method'=>'POST','enctype'=>'multipart/form-data'])!!}
             <div class="form-group">
                 {{Form::label('title','Title')}}
@@ -34,35 +33,5 @@
             </div>
             {{Form::submit('submit',['class'=>'btn btn-primary'])}}
         {!!Form::close()!!}
-=======
-        <div class="add-book-form">
-            {!!Form::open(['action'=>'BooksController@store','method'=>'POST','enctype'=>'multipart/form-data'])!!}
-                <div class="form-group addbook-formgroup">
-                    {{Form::label('title','Title')}}
-                    {{Form::text('bookTitle','',['class'=>'form-control addbook-textbox','placeholder'=>'Enter Book Title'])}}
-                </div>
-                <div class="form-group addbook-formgroup">
-                    {{Form::label('author','Author')}}
-                    {{Form::text('bookAuthor','',['class'=>'form-control addbook-textbox','placeholder'=>'Enter Book Author'])}}
-                </div>
-                <div class="form-group addbook-formgroup">
-                    {{Form::label('publisher','Publisher')}}
-                    {{Form::text('bookPublisher','',['class'=>'form-control addbook-textbox','placeholder'=>'Enter Book Author'])}}
-                </div>
-                <div class="form-group addbook-formgroup">
-                    {{Form::label('reference','Reference Number')}}
-                    {{Form::text('bookReference','',['class'=>'form-control addbook-textbox','placeholder'=>'Enter Book Author'])}}
-                </div>
-                <div class="form-group addbook-formgroup">
-                    {{Form::label('description','Description')}}
-                    {{Form::textarea('bookDescription','',['class'=>'form-control addbook-desc','placeholder'=>'Enter Book Author'])}}
-                </div>
-                <div class="form-group addbook-formgroup">
-                    {{Form::file('bookImage')}}
-                </div>
-                {{Form::submit('submit',['class'=>'btn btn-primary'])}}
-            {!!Form::close()!!}
-        </div>
->>>>>>> 191af9264b6f27f56adbc9242aefd621154d3baf
     </div>
 @endsection
