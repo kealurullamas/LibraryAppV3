0c5d1966f7258e92919ef03001165dd332f6dcec
gabdelgado <gabgrietadelgado@gmail.com>
2018-03-25 09:48:09 +0800

    added book desc

--- resources/views/Books/views.blade.php
+++ resources/views/Books/views.blade.php
@@ -1,15 +1,18 @@
 @extends('Layouts.app')
 
 @section('content')
-    <div class="Searchcontainer">
-        <img src="{{asset('storage/images/'.$books->image)}}">
-        <h1>{{$books->title}}</a></h1>
-        <h6>{{$books->author}}</h6>
-        <h5>{{$books->description}}</h5>
-        {!!Form::open(['action'=>'BookRequestsController@store' ,'method'=>'POST'])!!}
-            {{Form::hidden('bookid',$books->id)}}
-            {{Form::hidden('bookname',$books->title)}}
-            {{Form::submit('Submit',['class'=>'btn btn-primary'])}} 
-        {!!Form::close()!!}
+    <div class="bookcontainer">
+        <div class="book-desc">
+            <img class="center-img" src="{{asset('storage/images/'.$books->image)}}">
+        
+            <h1 class="book-title">{{$books->title}}</a></h1>
+            <h6 class="book-author">{{$books->author}}</h6>
+            <h5 class="book-description">{{$books->description}}</h5>
+            {!!Form::open(['action'=>'BookRequestsController@store' ,'method'=>'POST'])!!}
+                {{Form::hidden('bookid',$books->id)}}
+                {{Form::hidden('bookname',$books->title)}}
+                {{Form::submit('Submit',['class'=>'btn btn-primary'])}} 
+            {!!Form::close()!!}
+        </div>
     </div>
 @endsection
